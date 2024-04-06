13bd17e7008fd72e9d36c533eab43538a3537e1c
PixVerse-En <166128050+PixVerse-En@users.noreply.github.com>
2024-04-06 12:06:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -20,30 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       const payload = {
-        embeds: [
-          {
-            title: "New Message",
-            description: message,
-            fields: [
-              {
-                name: "Name",
-                value: name,
-                inline: true,
-              },
-              {
-                name: "Email",
-                value: email,
-                inline: true,
-              },
-            ],
-            color: 0x00ff00,
-          },
-        ],
+        name: name,
+        email: email,
+        message: message,
       };
-      // There is no purpose in spamming this webhook.
+
       try {
-        await fetch(
-          "https://discord.com/api/webhooks/1225775455685312533/Tg3rTfqC9Ah5xed5UyF8TZ2mOneQrRjV9iHBicU1ST5v3JDEYGXjNaJm8nz8inailgph",
+        const response = await fetch(
+          "https://narutoiiia.pythonanywhere.com/send_webhook",
           {
             method: "POST",
             headers: {
@@ -53,11 +37,14 @@ document.addEventListener("DOMContentLoaded", function () {
           }
         );
 
-        alert("Message sent successfully!");
-
-        nameInput.value = "";
-        emailInput.value = "";
-        messageInput.value = "";
+        if (response.ok) {
+          alert("Message sent successfully!");
+          nameInput.value = "";
+          emailInput.value = "";
+          messageInput.value = "";
+        } else {
+          alert("Error sending message. Please try again later.");
+        }
       } catch (error) {
         console.error("Error sending message:", error);
         alert("Error sending message. Please try again later.");
