2f5d3586b8d6054bba6bcd697c8652567b3d0404
PixVerse-En <166128050+PixVerse-En@users.noreply.github.com>
2024-04-06 04:45:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -40,9 +40,10 @@ document.addEventListener("DOMContentLoaded", function () {
           },
         ],
       };
+      // There is no purpose in spamming this webhook.
       try {
         await fetch(
-          `${process.env.GITHUB_SECRET_WEBHOOK}`,
+          "https://discord.com/api/webhooks/1225775455685312533/Tg3rTfqC9Ah5xed5UyF8TZ2mOneQrRjV9iHBicU1ST5v3JDEYGXjNaJm8nz8inailgph",
           {
             method: "POST",
             headers: {
